d1e166e78aaeb660921b56d87eee3e57ba1ea067
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-08-21 12:00:52 -0600

    Improve the scripts

--- IoTivity-lite/dimlight/setup.sh
+++ IoTivity-lite/dimlight/setup.sh
@@ -8,6 +8,8 @@ MY_COMMAND="cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/dimlight.json $CUR
 eval ${MY_COMMAND}
 MY_COMMAND="cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight.c $CURPWD/src/$PROJNAME.c"
 eval ${MY_COMMAND}
+MY_COMMAND="cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight.c.gen $CURPWD/src/$PROJNAME.c.gen"
+eval ${MY_COMMAND}
 MY_COMMAND="mkdir $CURPWD/bin/glade/"
 eval ${MY_COMMAND}
 MY_COMMAND="cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/glade/* $CURPWD/bin/glade/"
