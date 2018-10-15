30deedc8849f4615afb1b12bd424c9ff889e3d15
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-15 14:47:57 -0600

    fixed directory

--- IoTivity/dimlight/setup.sh
+++ IoTivity/dimlight/setup.sh
@@ -2,6 +2,6 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/dimlight/SConscript ${CURPWD}/
-cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/dimlight/example-config.json ${CURPWD}/${PROJNAME}-config.json
-cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/emulator/dimlight/example.json ${CURPWD}/${PROJNAME}.json
+cp ${EXAMPLEPATH}/Emulator-Code/IoTivity/dimlight/SConscript ${CURPWD}/
+cp ${EXAMPLEPATH}/Emulator-Code/IoTivity/dimlight/example-config.json ${CURPWD}/${PROJNAME}-config.json
+cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/example.json ${CURPWD}/${PROJNAME}.json
