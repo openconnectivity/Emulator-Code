dbd37a3bca5502a8e95044c5b9f1599df38d8ba2
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-15 11:42:05 -0600

    modified setup file

--- IoTivity-lite/dimlight/setup.sh
+++ IoTivity-lite/dimlight/setup.sh
@@ -3,6 +3,7 @@ CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/Makefile ${CURPWD}/
+cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/dimlight.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight.c ${CURPWD}/src/${PROJNAME}.c
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-gen.c ${CURPWD}/src/${PROJNAME}-gen.c
