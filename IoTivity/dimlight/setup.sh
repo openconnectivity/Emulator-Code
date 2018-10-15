097de5b80b84f2eced9552d86bf4a142c9020762
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-15 14:49:34 -0600

    name change

--- IoTivity/dimlight/setup.sh
+++ IoTivity/dimlight/setup.sh
@@ -3,5 +3,5 @@ CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity/dimlight/SConscript ${CURPWD}/
-cp ${EXAMPLEPATH}/Emulator-Code/IoTivity/dimlight/example-config.json ${CURPWD}/${PROJNAME}-config.json
-cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/example.json ${CURPWD}/${PROJNAME}.json
+cp ${EXAMPLEPATH}/Emulator-Code/IoTivity/dimlight/dimlight-config.json ${CURPWD}/${PROJNAME}-config.json
+cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/dimlight.json ${CURPWD}/${PROJNAME}.json
