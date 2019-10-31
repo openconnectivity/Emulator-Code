35054f4932a004a3ff8ad1fdea1b1b219834c7b7
Clarke Stevens <clarke.stevens@sjrb.ca>
2019-10-31 11:08:36 -0600

    updated to latest IoTivity

--- IoTivity-lite/dimlight/setup.sh
+++ IoTivity-lite/dimlight/setup.sh
@@ -2,10 +2,10 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/Makefile ${CURPWD}/
+cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/devbuildmake ${CURPWD}/devbuildmake
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/dimlight.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight.c ${CURPWD}/src/${PROJNAME}.c
-cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-gen.c ${CURPWD}/src/${PROJNAME}-gen.c
+cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-gen.c ${CURPWD}/src/${PROJNAME}-old.c
 mkdir ${CURPWD}/bin/glade/
 cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/glade/* ${CURPWD}/bin/glade/
